031e59950fb95ea11ff95da900d6314bc94ce756
chachouman221205 <48631302+chachouman221205@users.noreply.github.com>
2022-04-15 08:34:00 +0200

    fix message d'erreur
    (grid() ne fonctionne pas avec pack() )

--- erreur.py
+++ erreur.py
@@ -4,5 +4,5 @@ def erreur(titre, message):
     fen = Tk()
     fen.title(titre)
     fen.geometry('400x150')
-    Label(text=message).grid(row=2,column=2)
+    Label(fen, text=message).grid(row=2,column=2)
     fen.mainloop()
