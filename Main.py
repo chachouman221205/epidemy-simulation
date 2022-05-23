a579d6cd051581d8d07ad737d0551cda52f93a85
LekubKisot <101388785+LekubKisot@users.noreply.github.com>
2022-05-23 15:08:30 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -423,7 +423,7 @@ historique = {}
 couleurs = {"M":'black',"R":"light grey","S":"white","V":"blue"} 
 case_libre = []
 mode = 0
-flag = Flase
+flag = False
 taille_cellule = 500//max(taille)
 flag_vaccination = False
 
